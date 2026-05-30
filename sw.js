0a18be223c739ed710a22bd72ef0436cd1c42ab6
weiweiouyangweiweiouyang-5114 <ouyangweiwei.ouyangweiwei@gmail.com>
2026-05-30 21:51:47 +0800

    Fix splash animation: use explicit animation properties

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dice-decision-app-v3";
+const CACHE_NAME = "dice-decision-app-v4";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
